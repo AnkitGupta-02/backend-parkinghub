dc18d23d0978aecce0d5e9bd8c1515a65d22786c
Ankit Gupta <ankitgupta310802@gmail.com>
2024-11-09 10:54:02 +0530

    .

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 const PORT = process.env.PORT;
-const pool = require('./config/db.js')
+
 // Import routes
-// const userRoute = require("./routes/userRoute.js");
-// const spotRoute = require("./routes/spotRoute.js");
+const userRoute = require("./routes/userRoute.js");
+const spotRoute = require("./routes/spotRoute.js");
 
 // Middleware setup
 app.use(express.json());
@@ -15,8 +15,8 @@ app.use(cors());
 
 // Route handling
 app.get("/", (req, res) => res.send("Home Page"));
-// app.use("/api/users", userRoute);
-// app.use("/api/spots", spotRoute);
+app.use("/api/users", userRoute);
+app.use("/api/spots", spotRoute);
 
 
 // 404 handler for unmatched routes
@@ -31,7 +31,6 @@ app.use((err, req, res, next) => {
 });
 
 // Server initialization
-console.log("db was connected :",pool.pool.config.connectionConfig.user);
 app.listen(PORT, () => {
   console.log(`Server is listning on ${PORT}`);
 });
